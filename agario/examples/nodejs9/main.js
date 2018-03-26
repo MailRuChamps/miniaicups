4d500b1a42bd4c242025d24c34c21542a8e748a3
Kislenko Maksim <m.kislenko@corp.mail.ru>
2018-03-26 17:23:32 +0300

    Interpret langs

---
+++ agario/examples/nodejs9/main.js
@@ -0,0 +1,35 @@
+var readline = require('readline');
+
+var iface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+iface.on('line', function (line) {
+	var parsed = JSON.parse(line);
+	var command = JSON.stringify(onTick(parsed));
+	console.log(command);
+});
+
+function onTick(parsed) {
+	if (parsed.Mine.length > 0) {
+		var mine = parsed.Mine[0];
+		var goal = findFood(parsed.Objects);
+		if (!!goal) {
+            return {X: goal.X, Y: goal.Y};
+		}
+		return {X: 0, Y: 0, Debug: 'No food'};
+	}
+	return {X: 0, Y: 0, Debug: 'Died'};
+}
+
+function findFood(objects) {
+	var food = objects.filter(function (obj) {
+		return obj.T == 'F';
+	});
+	if (food.length > 0) {
+		return food[0];
+	}
+	return null;
+}
