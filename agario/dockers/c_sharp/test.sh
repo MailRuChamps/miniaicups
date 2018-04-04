ac9b31017fce2526bfa57f72e8fbeed7ca778ff4
OlegJakushkin <oleg.jakushkin@gmail.com>
2018-04-05 01:33:24 +0300

    docker build fix

--- agario/dockers/c_sharp/test.sh
+++ agario/dockers/c_sharp/test.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-docker build -t cups/csharp:latest ./  \
+docker build -t cups/csharp:latest ./
 timeout 5s docker run -it -e MOUNT_POINT=/opt/client/csharpStrategy  \
     -e SOLUTION_CODE_PATH='/opt/client/'  \
     -v ../../examples/csharp:/opt/client/code  \
