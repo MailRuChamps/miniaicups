746bb2a593c785246df5adc5735b8e540ec1e45a
OlegJakushkin <oleg.jakushkin@gmail.com>
2018-04-05 01:30:05 +0300

    travis for csharp docker
    travis docker test

---
+++ agario/dockers/c_sharp/test.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+docker build -t cups/csharp:latest ./  \
+timeout 5s docker run -it -e MOUNT_POINT=/opt/client/csharpStrategy  \
+    -e SOLUTION_CODE_PATH='/opt/client/'  \
+    -v ../../examples/csharp:/opt/client/code  \
+    -v ../../examples/build:/opt/client/  \
+    cups/csharp  \
+    bash -c "eval \$RUN_COMMAND"  \
+    
