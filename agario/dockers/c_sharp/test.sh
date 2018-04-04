3d158ac4f5de9f0c9dae5520b8d05c22011acf7d
OlegJakushkin <oleg.jakushkin@gmail.com>
2018-04-05 01:44:54 +0300

    update docker build for CSharp and travis

--- agario/dockers/c_sharp/test.sh
+++ agario/dockers/c_sharp/test.sh
@@ -1,9 +1,12 @@
 #!bin/bash
+CODE_DIR=$(echo "$(cd "../../examples/csharp"; pwd)")
+BUILD_DIR=$(echo "$(cd "../../examples/"; pwd)")
+
 docker build -t cups/csharp:latest ./
 timeout 5s docker run -it -e MOUNT_POINT=/opt/client/csharpStrategy  \
     -e SOLUTION_CODE_PATH='/opt/client/'  \
-    -v ../../examples/csharp:/opt/client/code  \
-    -v ../../examples/build:/opt/client/  \
+    -v $CODE_DIR:/opt/client/code  \
+    -v $BUILD_DIR:/opt/client/  \
     cups/csharp  \
     bash -c "eval \$RUN_COMMAND"  \
     
