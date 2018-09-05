9a4e9737f6e7786a12a8cb5c4c8a1d496a4a82a5
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2018-09-06 01:57:04 +0300

    Simplify Drawer code
    Signed-off-by: Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>

--- madcars/Runners/visual/drawer.py
+++ madcars/Runners/visual/drawer.py
@@ -1,11 +1,8 @@
 class Drawer:
 	def __init__(self, color=0x000000, layer=3):
-		self.objects = []
-		self.x = 0
-		self.y = 0
-
-		self.layer = layer
-		self.color = color
+		self.reset()
+		self.x, self.y = 0, 0
+		self.color, self.layer = color, layer
 
 	def reset(self):
 		self.objects = []
@@ -13,8 +10,8 @@ class Drawer:
 	def move(self, x, y):
 		self.x, self.y = x, y
 
-	def add_object(self, obj):
-		self.objects.append(obj)
+	def add(self, drawer):
+		self.objects.append(drawer)
 
 	def draw(self, rewind):
 		for obj in self.objects:
