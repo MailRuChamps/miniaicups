c43a57bb1afa5188b91e0e866ccf63262e004e6e
AlexHell <alexey-polyakov@mail.ru>
2019-08-02 01:33:13 +0300

    расчет времени и выдача 'time_left' в FileClient, рефакторинг (TcpClient и FileClient от базового класса)

--- paperio/local_runner/clients.py
+++ paperio/local_runner/clients.py
@@ -147,13 +147,41 @@ class SimplePythonClient(Client):
             pass
 
 
-class TcpClient(Client):
-    EXECUTION_LIMIT = datetime.timedelta(seconds=MAX_EXECUTION_TIME)
+class BasicProxyClient(Client):
+    REQUEST_LIMIT = datetime.timedelta(seconds=REQUEST_MAX_TIME)
+    TOTAL_LIMIT = datetime.timedelta(seconds=MAX_EXECUTION_TIME)
+
+    def __init__(self):
+        self.execution_time = datetime.timedelta()
+        self.started_measure_time = None
+
+    def prepare_message_bytes(self, t, d):
+        msg = {
+            'type': t,
+            'params': d,
+            'time_left': round((self.TOTAL_LIMIT - self.execution_time).total_seconds() * 1000)
+        }
+        return '{}\n'.format(json.dumps(msg)).encode()
+
+    def begin_measure_time(self):
+        self.started_measure_time = datetime.datetime.now()
+
+    def end_measure_time(self):
+        request_time = (datetime.datetime.now() - self.started_measure_time)
+        if request_time > self.REQUEST_LIMIT:
+            raise Exception('request timeout error')
+
+        self.execution_time += request_time
+        if self.execution_time > self.TOTAL_LIMIT:
+            raise Exception('sum timeout error')
+
+
+class TcpClient(BasicProxyClient):
 
     def __init__(self, reader, writer):
+        super(TcpClient, self).__init__()
         self.reader = reader
         self.writer = writer
-        self.execution_time = datetime.timedelta()
         self.solution_id = None
 
     def save_log_to_disk(self, log, path):
@@ -178,31 +206,25 @@ class TcpClient(Client):
         return bool(self.solution_id)
 
     def send_message(self, t, d):
-        msg = {
-            'type': t,
-            'params': d,
-            'time_left': round((self.EXECUTION_LIMIT-self.execution_time).total_seconds()*1000)
-        }
-        msg_bytes = '{}\n'.format(json.dumps(msg)).encode()
+        msg_bytes = self.prepare_message_bytes(t, d)
         self.writer.write(msg_bytes)
 
     async def get_command(self):
         try:
-            before = datetime.datetime.now()
+            self.begin_measure_time()
+            #
             z = await asyncio.wait_for(self.reader.readline(), timeout=REQUEST_MAX_TIME)
             if not z:
                 raise ConnectionError('Connection closed')
-            self.execution_time += (datetime.datetime.now() - before)
-            if self.execution_time > self.EXECUTION_LIMIT:
-                raise Exception('sum timeout error')
+            #
+            self.end_measure_time()
         except asyncio.TimeoutError:
             raise asyncio.TimeoutError('read timeout error')
-        try:
-            z = json.loads(z.decode())
-        except ValueError:
-            z = {'debug': 'cant pars json'}
 
-        return z
+        try:
+            return json.loads(z.decode())
+        except ValueError:
+            return {'debug': 'cant pars json'}
 
     def close(self):
         self.writer.close()
@@ -211,8 +233,10 @@ class TcpClient(Client):
         return self.solution_id
 
 
-class FileClient(Client):
+class FileClient(BasicProxyClient):
+
     def __init__(self, path_to_script, path_to_log=None):
+        super(FileClient, self).__init__()
         self.process = Popen(path_to_script, stdout=PIPE, stdin=PIPE)
         self.last_message = None
         if path_to_log is None:
@@ -223,20 +247,19 @@ class FileClient(Client):
             self.path_to_log = path_to_log
 
     def send_message(self, t, d):
-        msg = {
-            'type': t,
-            'params': d
-        }
-        msg_bytes = '{}\n'.format(json.dumps(msg)).encode()
-
+        msg_bytes = self.prepare_message_bytes(t, d)
         self.process.stdin.write(msg_bytes)
         self.process.stdin.flush()
 
     async def get_command(self):
         try:
-            line = self.process.stdout.readline().decode('utf-8')
-            state = json.loads(line)
-            return state
+            self.begin_measure_time()
+            #
+            z = self.process.stdout.readline().decode('utf-8')
+            #
+            self.end_measure_time()
+
+            return json.loads(z)
         except Exception as e:
             return {'debug': str(e)}
 
