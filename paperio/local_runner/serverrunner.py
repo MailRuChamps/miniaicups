e73925bbdd558d9c1c0b6edecf03de344ffcd8e7
Kislenko Maksim <m.kislenko@corp.mail.ru>
2019-08-07 17:18:18 +0300

    Добавлен Dockerfile и serverrunner

---
+++ paperio/local_runner/serverrunner.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from clients import TcpClient
+from game_objects.game import Game
+from constants import CONSTS
+
+
+class GameServer:
+    def __init__(self):
+        self.clients = []
+
+    async def connection_handler(self, client_reader, client_writer):
+        client = TcpClient(client_reader, client_writer)
+        is_success = await client.set_solution_id()
+
+        clients_count = len(self.clients)
+
+        if clients_count < CONSTS.CLIENTS_COUNT:
+            if is_success:
+                clients_count += 1
+                print('{} clients connected'.format(clients_count))
+                self.clients.append(client)
+            else:
+                loop.stop()
+        else:
+            client_writer.close()
+
+        game_future = None
+        if clients_count == CONSTS.CLIENTS_COUNT:
+            game = Game(self.clients)
+            game_future = asyncio.ensure_future(game.game_loop_wrapper())
+
+        if game_future:
+            done, pending = await asyncio.wait([game_future])
+            if not pending:
+                loop.stop()
+
+            print('game done')
+
+
+gs = GameServer()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.start_server(gs.connection_handler, '0.0.0.0', 8000))
+try:
+    loop.run_forever()
+finally:
+    loop.close()
